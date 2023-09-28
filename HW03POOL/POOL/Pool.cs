94faf7fb8ee8141f383fb2badc578bc5ece26d1e
konnnst <k.ovsyannikov420@gmail.com>
2023-09-28 20:35:10 +0300

    Pool project created

---
+++ HW03POOL/POOL/Pool.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+public class Task<TResult>
+{
+    private Func<TResult> Func;
+    public bool IsCompleted = false;
+    public TResult? Result;
+
+    public Task(Func<TResult> func)
+    {
+        Func = func;
+    }
+}
+public class MyThreadPool
+{
+    private Thread[] Threads;
+    private Queue<int> FreeThreadQueue;
+    private Queue<Task<object>>? TaskQueue;
+    public MyThreadPool(int threadCount)
+    {
+        FreeThreadQueue = new Queue<int>();
+        Threads = new Thread[threadCount];
+        for (int i = 0; i < threadCount; ++i)
+        {
+            Threads[i] = new Thread(() => WaitForTask());
+            FreeThreadQueue.Enqueue(i);
+        }
+    }
+
+    private void WaitForTask()
+    {
+        while (true)
+            Thread.Yield();
+    }
+
+}
+
+internal class Program
+{
+    void Main()
+    {
+
+    }
+}
