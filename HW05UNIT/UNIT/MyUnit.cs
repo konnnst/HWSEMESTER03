9d6cc59acda0c430697e20bf27cd14e145af444c
konstantin.ovsyannikov <st107930@gmail.com>
2023-11-23 11:36:55 +0300

    add: initial commit

---
+++ HW05UNIT/UNIT/MyUnit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace Unit;
+
+
+class MyUnit
+{
+    static void Main(string[] args)
+    {
+        if (args.Count<string>() == 1)
+        {
+            Console.WriteLine("Running tests");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect arguments count");
+        }
+
+        Console.Read();
+    }
+}
